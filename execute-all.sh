b3ed9fa1b2f507c6f81b9d12afc9d1848d67ca24
André Felipe Guimarães <andrefg.dev@gmail.com>
2024-12-05 00:01:35 -0300

    final version

--- execute-all.sh
+++ execute-all.sh
@@ -3,9 +3,9 @@
 bash scripts/install-dependencies.sh
 bash scripts/get-reads.sh
 bash scripts/get-qualities.sh
-bash scripts/filter-reads.sh
+bash script/filter-reads.sh data/reads/SRR14457781.fastq
 bash scripts/index-reference.sh
-bash scripts/align_reads.sh
+bash scripts/align_reads.sh data/reads/SRR1570792.fastq data/filtered_reads/SRR14457781/good/SRR14457781_filtered_Q30_30.fastq 
 bash scripts/normalize-bams.sh
 bash scripts/bam-analysis.sh
 bash scripts/identify-variants.sh
--- scripts/bam-analysis.sh
+++ scripts/bam-analysis.sh
@@ -12,10 +12,23 @@ mkdir -p "$OUTPUT_DIR"
 for bam_file in "$INPUT_DIR"/*.bam; do
     # Extrai o nome base do arquivo BAM sem a extensão
     base_name=$(basename "$bam_file" .bam)
-    
-    # Executa o comando flagstat e redireciona para o arquivo de saída
-    tools/samtools/samtools flagstat "$bam_file" >> "$OUTPUT_DIR/${base_name}.flag"
+
+    OUTPUT_LOG_DEPTH="$OUTPUT_DIR/log_depth_${base_name}.txt"    
     
     # Executa o comando depth e redireciona para o arquivo de log de profundidade
-    tools/samtools/samtools depth "$bam_file" >> "$OUTPUT_DIR/log_depth_${base_name}.txt"
+    tools/samtools/samtools depth "$bam_file" >> $OUTPUT_LOG_DEPTH
+
+    # Conta as linhas do arquivo de log e faz o cálculo
+    line_count=$(wc -l < "$OUTPUT_LOG_DEPTH")
+
+    # Realiza o cálculo (Valor do wc -l / 12157105) * 100
+    result=$(echo "scale=4; ($line_count / 12157105) * 100" | bc)
+
+    # Cria o arquivo de resultado e exporta o cálculo de cobertura por extensão
+    OUTPUT_RESULT="$OUTPUT_DIR/${base_name}_extension_coverage.txt"
+    echo "$result" > "$OUTPUT_RESULT"
+
+    # Cria o arquivo de resultado e exporta o cálculo de cobertura em profundidade
+    awk '{total += $3; count++} END { print total/count}' "$OUTPUT_LOG_DEPTH" > "$OUTPUT_DIR/${base_name}_depth_coverage.txt"
+
 done
